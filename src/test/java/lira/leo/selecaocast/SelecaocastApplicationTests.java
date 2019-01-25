41ec07d0099eb56df74384b18197a344d2cf1aca
leonardo.lira <leonardo.lira@qsintelligence.com.br>
2019-01-24 22:05:13 -0300

    Ajustes

--- src/test/java/lira/leo/selecaocast/SelecaocastApplicationTests.java
+++ src/test/java/lira/leo/selecaocast/SelecaocastApplicationTests.java
@@ -6,12 +6,14 @@ import org.junit.runners.Suite;
 import lira.leo.selecaocast.questao1.Questao1ApplicationTests;
 import lira.leo.selecaocast.questao1.service.Questao1ServiceTest;
 import lira.leo.selecaocast.questao2.Questao2Test;
+import lira.leo.selecaocast.questao3.Questao3ApplicationTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 		Questao1ApplicationTests.class,
 		Questao1ServiceTest.class,
-		Questao2Test.class
+		Questao2Test.class,
+		Questao3ApplicationTest.class
 })
 public class SelecaocastApplicationTests {
 
